08753e9744abe30b5b495b1b425414031593d75c
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-02 07:30:21 -0300

    First commit task list exercise

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 📝 Lista de tareas
+// Crear un programa que tenga un menú con las siguientes opciones:
+// AGREGAR TAREA: debe pedir al usuario ingresar una tarea y agregarla a la lista de tareas
+// MODIFICAR TAREA: debe pedir al usuario ingresar el número de tarea que se desea modificar, mostrarla, y pedir que ingrese la tarea a reemplazarla o modificarla
+// ELIMINAR TAREA: debe pedir al usuario ingresar el número de tarea a eliminar, mostrarla, y pedirle confirmar si desea eliminarla o no
+// VER TAREAS: debe listar todas las tareas hasta el momento
+// SALIR: debe terminar la ejecución del programa
+// El programa debe mostrar el menú de opciones, y permitir ingresar una opción. Si la opción ingresada es incorrecta, debe mostrar nuevamente el menú. Si se elige SALIR, debe terminar el programa. Si se elige alguna de las otras opciones, debe realizar las acciones correspondientes, y una vez terminada, volver al menú de opciones.
